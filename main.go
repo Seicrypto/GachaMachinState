09b789f3d86284e5e340be41f37153793ffa96f1
Sei <97655693+Seicrypto@users.noreply.github.com>
2024-03-02 21:39:30 +0800

    Create execution - main.go

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"exmaple/sei/gachaMechineState/internal/gachaMachin"
+	"exmaple/sei/gachaMechineState/internal/manager"
+	"exmaple/sei/gachaMechineState/internal/orderState"
+	"fmt"
+	"sync"
+)
+
+func main() {
+	gacha := gachaMachin.NewGachaContext()
+	manager := manager.NewManager(gacha)
+
+	var wg sync.WaitGroup
+	for i := 0; i < 20; i++ { // 測試有20張訂單併發
+		wg.Add(1)
+		go func(orderNum int) {
+			defer wg.Done()
+			order := orderState.NewOrderContext()
+			order.OrderTarget = "gacha"
+			manager.HandleOrder(order)
+			fmt.Printf("Order %d completed\n", orderNum)
+		}(i)
+	}
+	wg.Wait() // 等待所有订单处理完毕
+}
